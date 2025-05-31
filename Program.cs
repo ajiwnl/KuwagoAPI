2945917850ed0e8f1750b09cd353a6cdac7c6647
ajaraneta <alvinjohnaranetz@gmail.com>
2025-06-01 03:08:49 +0800

    AJ-added swagger live deployment

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ var app = builder.Build();
 startup.Configure(app, app.Environment);
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
