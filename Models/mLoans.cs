75a232d355b8bf2548142cf0db6c723af0fd1586
ajaraneta <alvinjohnaranetz@gmail.com>
2025-06-21 23:56:28 +0800

    POST request Loan working

---
+++ Models/mLoans.cs
@@ -0,0 +1,40 @@
+﻿using Google.Cloud.Firestore;
+using static KuwagoAPI.Helper.LoanEnums;
+
+namespace KuwagoAPI.Models
+{
+    [FirestoreData]
+    public class mLoans
+    {
+        [FirestoreProperty]
+        public string UID { get; set; }
+
+        [FirestoreProperty]
+        public string MaritalStatus { get; set; }
+
+        [FirestoreProperty]
+        public string HighestEducation { get; set; }
+
+        [FirestoreProperty]
+        public string EmploymentInformation { get; set; }
+
+        [FirestoreProperty]
+        public string DetailedAddress { get; set; }
+
+        [FirestoreProperty]
+        public string ResidentType { get; set; }
+
+        [FirestoreProperty]
+        public string LoanType { get; set; }
+
+        [FirestoreProperty]
+        public int LoanAmount { get; set; }
+
+        [FirestoreProperty]
+        public string LoanPurpose { get; set; }
+
+        [FirestoreProperty]
+        public Timestamp CreatedAt { get; set; } = Timestamp.FromDateTime(DateTime.UtcNow);
+    }
+
+}
